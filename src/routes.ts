a3ff6af5201c1776cbda7f48c082cfc22f767b41
Guilherme Maffei <guilhermemaffeidev@gmail.com>
2024-09-16 01:11:33 -0300

    changes

---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+// import authRoutes from './modules/auth/auth.routes';
+// import companyRoutes from './modules/companies/companies.routes';
+
+const router = Router();
+
+// router.use('/auth', authRoutes);
+// router.use('/companies', companyRoutes);
+
+// Adicione as rotas para outros módulos aqui
+
+export default router;
---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import router from "./routes";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use("/api", router);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
